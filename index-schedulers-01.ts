0c0073134593b152fe18eb6a00a741a9cce3126b
Kiran Anand <liteninkiran@hotmail.com>
2023-10-28 00:27:24 +0100

    Execute tasks asynchronously with Async Scheduler

---
+++ index-schedulers-01.ts
@@ -0,0 +1,62 @@
+import './styles.css';
+import { asyncScheduler, of, fromEvent } from 'rxjs';
+import { observeOn, subscribeOn, tap } from 'rxjs/operators';
+
+const observer = {
+    next: val => console.log('next', val),
+    error: err => console.log('error', err),
+    complete: () => console.log('complete'),
+};
+
+/*
+ * The asyncScheduler lets you schedule tasks asynchronously,
+ * similar to a setTimeout. All schedulers have a signature
+ * of work, delay, state, but providing a delay for any other
+ * scheduler will simply default it to the asyncScheduler
+ * behind the scenes.
+ * 
+ * The schedule call returns a subscription, so if you need
+ * to cancel work before it is performed you can simply
+ * unsubscribe, similar to observables.
+ */
+// const sub = asyncScheduler.schedule(console.log, 2000, 'Hello World!');
+// console.log('Sync');
+// // sub.unsubscribe();
+
+/*
+ * Most static creation operators accept a scheduler as 
+ * the last argument. For instance, if we want to emit
+ * values from of asynchronously, we can supply the
+ * asyncScheduler as the last argument. 
+ * 
+ * Output: 4,5,6,1,2,3
+ */
+// of(1,2,3, asyncScheduler).subscribe(observer);
+// of(4,5,6).subscribe(observer);
+// console.log('Sync');
+
+/*
+ * You can also introduce schedulers by using the 
+ * observeOn operator. This is equivalent to wrapping
+ * next, error, and complete functions in appropriate
+ * scheduler.
+ */
+// of(1, 2, 3).pipe(
+//     // Logging values before scheduler
+//     tap(observer),
+//     // Delay can also be supplied as second argument
+//     observeOn(asyncScheduler, 2000)
+// ).subscribe(observer);
+// console.log('Sync');
+
+/*
+ * Lastly, you can use schedulers to determine when
+ * subscriptions occur by using the subscribeOn 
+ * operator. This is equivalent to wrapping your entire 
+ * subscription in a scheduler.
+ */
+of(1, 2, 3).pipe(
+    tap(observer),
+    subscribeOn(asyncScheduler, 2000)
+).subscribe(observer);
+console.log('Sync');
